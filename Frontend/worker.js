56019f87ba100ba26dc9fb3e2023d53b836b65f8
Shubham Kumar <aryanshara41@gmail.com>
2023-05-11 11:30:13 +0530

    second commit

--- Frontend/worker.js
+++ Frontend/worker.js
@@ -7,6 +7,31 @@ async function fetchData() {
 
 fetchData();
 
+const circularProgress = document.querySelectorAll(".circular-progress");
+
+Array.from(circularProgress).forEach((progressBar) => {
+    const progressValue = progressBar.querySelector(".percentage");
+    const innerCircle = progressBar.querySelector(".inner-circle");
+    let startValue = 0,
+        endValue = 60,
+        speed = 1000,
+        progressColor = "#3DC6C1"
+    const progress = setInterval(() => {
+        startValue++;
+        progressValue.textContent = `${startValue}`;
+        progressValue.style.color = `${progressColor}`;
+
+        innerCircle.style.backgroundColor = "#191D28"
+
+        progressBar.style.background = `conic-gradient(${progressColor} ${ startValue * 6
+            }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
+        if (startValue === endValue) {
+            clearInterval(progress);
+        }
+    }, speed);
+});
+
+
 function showDropDown(e) {
     if (e == null) return;
 
